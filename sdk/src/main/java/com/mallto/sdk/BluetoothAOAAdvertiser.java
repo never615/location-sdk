4bb69ceac314ea9acda13e38b3602f7b32715710
shingkit <sunshengjie@fanle.com>
2024-08-29 02:13:50 +0800

    AOA协议

--- sdk/src/main/java/com/mallto/sdk/BluetoothAOAAdvertiser.java
+++ sdk/src/main/java/com/mallto/sdk/BluetoothAOAAdvertiser.java
@@ -75,22 +75,44 @@ public class BluetoothAOAAdvertiser {
 //            return new byte[]{
 ////                (byte) 0x42,
 ////                (byte) 0x25,
-////                (byte) 0xAA,(byte) 0xBB,(byte) 0xCC,(byte) 0xDD,(byte) 0xEE,(byte) 0xFF, (byte) 0x1E,
+////                (byte) 0xAA,(byte) 0xBB,(byte) 0xCC,(byte) 0xDD,(byte) 0xEE,(byte) 0xFF,
+// (byte) 0x1E,
 ////                (byte) 0xFF,
-////                (byte) 0x01,(byte) 0x00,(byte) 0x01,(byte) 0x00, (byte) 0x00,(byte) 0x00,(byte) 0x00,(byte) 0x00,
-////                (byte) 0xFD,
+////                (byte) 0x01,(byte) 0x00,(byte) 0x01,(byte) 0x00, (byte) 0x00,(byte) 0x00,(byte) 0x00,
+////               (byte) 0x00, (byte) 0xFD, // crc16
 ////        };
     private byte[] getManufacturerDataBytes() {
 
+        ByteBuffer advA_deviceInfo = ByteBuffer.allocate(15);
+        advA_deviceInfo.put(new byte[]{ // advA 应该是固定的吧
+                (byte) 0xAA,
+                (byte) 0xBB,
+                (byte) 0xCC,
+                (byte) 0xDD,
+                (byte) 0xEE,
+                (byte) 0xFF,
+        });
         ByteBuffer manufacturerData = ByteBuffer.allocate(31);
-        // todo 没看懂 状态、配置信息分配表，从图里拿到7bytes的信息
+        // 状态、配置信息分配表，从图里拿到7bytes的信息
+        // update By filling in cmd=0xBB, all other fields can be filled with 0x00, 先用0xBB填充
         byte[] deviceInfo = new byte[]{
+                (byte) 0xBB, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00
+        };
+        manufacturerData.put(0, (byte) 0x1E); // 长度30
+        manufacturerData.put((byte) 0xFF); // type
+        manufacturerData.put(deviceInfo); // device info 7 bytes
 
-        };
-        // todo 需要计算
-        byte[] crc16 = new byte[]{
-                (byte) 0x00, (byte) 0xFD
-        };
+        advA_deviceInfo.put(manufacturerData.array());
+
+        int crc16 = CRC16.crc16(advA_deviceInfo.array(), 15, 0XFFFF);
+
+// 将 int 类型的 CRC 值转换为 byte 数组
+        byte[] crcBytes = new byte[2];
+        crcBytes[0] = (byte) (crc16 >> 8);
+        crcBytes[1] = (byte) crc16;
+
+        manufacturerData.put(crcBytes); // crc16
+
         byte[] cteInfo = new byte[]{
                 (byte) 0x50, (byte) 0xbd, (byte) 0x84, (byte) 0xb1, // CTE Info
                 (byte) 0x32, (byte) 0x9F, (byte) 0x14, (byte) 0x9d,
@@ -98,11 +120,6 @@ public class BluetoothAOAAdvertiser {
                 (byte) 0x0f, (byte) 0x38, (byte) 0x72, (byte) 0x2d,
                 (byte) 0xa8, (byte) 0x5e, (byte) 0xc2, (byte) 0x58
         };
-//
-        manufacturerData.put(0, (byte) 0x1E); // 长度30
-        manufacturerData.put((byte) 0x02); // type todo 值是啥？
-        manufacturerData.put(deviceInfo); // device info 7 bytes
-        manufacturerData.put(crc16); // crc16
         manufacturerData.put(cteInfo); // cte 固定
         return manufacturerData.array();
     }
